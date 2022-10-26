c2e26da8e5c8f827ad573395bb7a357746296fff
jcost <jcost@users.noreply.github.com>
2022-10-25 23:38:44 -0400

    Add initial test to repo

---
+++ ApiMethods/OppFiApi.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OppFiAssessment.ApiMethods
+{
+    public class OppFiApi
+    {
+
+    }
+}
---
+++ Tests/OppFiDemoApiTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OppFiAssessment.Tests
+{
+    [TestClass]
+    public class OppFiDemoApiTests
+    {
+        [TestMethod]
+        public void OfferAccepted()
+        {
+            Assert.AreEqual(1, 1);
+
+        }
+        [TestMethod]
+        public void OfferDeclined()
+        {
+            Assert.AreEqual(1, 1);
+
+        }
+        [TestMethod]
+        public void OfferNoDecision()
+        {
+            Assert.AreEqual(1, 1);
+
+        }
+    }
+}
